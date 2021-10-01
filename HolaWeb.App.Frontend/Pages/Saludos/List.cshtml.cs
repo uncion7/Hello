16dd0a452208457bc12319883835468b06777ed5
uncion7 <uncion7@gmail.com>
2021-10-01 17:19:30 -0500

    Primera Versión del Programa

---
+++ HolaWeb.App.Frontend/Pages/Saludos/List.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HolaWeb.App.Frontend.Pages
+{
+    public class ListModel : PageModel
+    {
+        private string[] saludos = {"Buenos días", "Buenas tardes", "Buenas noches", "Hasta mañana"};
+
+        public List<string> ListaSaludos{get; set;}
+
+        public void OnGet()
+        {
+            ListaSaludos = new List<string>();
+            ListaSaludos.AddRange(saludos);
+        }
+    }
+}
